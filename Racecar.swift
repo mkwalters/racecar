a516ea4cc1ad335a64d1438560f598e8824d6604
Mitchell <mitchell@shopkeepery>
2017-03-21 15:31:44 -0700

    added x acceleration

---
+++ Racecar.swift
@@ -0,0 +1,24 @@
+//
+//  RaceCar.swift
+//  racecar
+//
+//  Created by Walters Mitch on 3/21/17.
+//  Copyright © 2017 Mitchell Walters. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Racecar: SKSpriteNode {
+    
+    
+    var x_position = 0
+    var y_position = 0
+    
+    var x_velocity = 0
+    var y_velocity = 0
+    
+    var x_acceleration = 0
+    var y_acceleration = 0
+    
+}
--- racecar/GameScene.swift
+++ racecar/GameScene.swift
@@ -17,94 +17,118 @@ class GameScene: SKScene {
     private var lastUpdateTime : TimeInterval = 0
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
+    let x_acceleration_button = SKSpriteNode(color: SKColor.red, size: CGSize(width: 175, height: 88))
+    
+    let x_deacceleration_button = SKSpriteNode(color: SKColor.blue, size: CGSize(width: 175, height: 88))
+    
+    
+    let y_acceleration_button = SKSpriteNode(color: SKColor.red, size: CGSize(width: 175, height: 88))
+    
+    let y_deacceleration_button = SKSpriteNode(color: SKColor.blue, size: CGSize(width: 175, height: 88))
+    
+    
+    let racecar = Racecar()
     
     override func sceneDidLoad() {
-
-        self.lastUpdateTime = 0
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        let x_acceleration_button = SKSpriteNode(color: SKColor.red, size: CGSize(width: 125, height: 88))
+        x_acceleration_button.position = CGPoint(x: -self.frame.width / 3, y: -1 * self.frame.height / 2 + 50)
+        x_acceleration_button.name = "x_acceleration_button"
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        let x_deacceleration_button = SKSpriteNode(color: SKColor.blue, size: CGSize(width: 125, height: 88))
+        x_deacceleration_button.position = CGPoint(x: -self.frame.width / 8, y: -1 * self.frame.height / 2 + 50)
+        x_deacceleration_button.name = "x_deacceleration_button"
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        let y_acceleration_button = SKSpriteNode(color: SKColor.red, size: CGSize(width: 125, height: 88))
+        y_acceleration_button.position = CGPoint(x: self.frame.width / 8, y: -1 * self.frame.height / 2 + 50)
+        y_acceleration_button.name = "y_acceleration_button"
+        
+        let y_deacceleration_button = SKSpriteNode(color: SKColor.blue, size: CGSize(width: 125, height: 88))
+        y_deacceleration_button.position = CGPoint(x: self.frame.width / 3, y: -1 * self.frame.height / 2 + 50)
+        y_deacceleration_button.name = "y_deacceleration_button"
+        
+        
+        
+        self.addChild(x_acceleration_button)
+        self.addChild(x_deacceleration_button)
+        self.addChild(y_acceleration_button)
+        self.addChild(y_deacceleration_button)
+        
+        if let grid = Grid(blockSize: 120.0, rows:9, cols:5) {
+            grid.position = CGPoint (x:frame.midX, y:frame.midY)
+            addChild(grid)
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+            let gamePiece = SKSpriteNode(imageNamed: "Spaceship")
+            gamePiece.setScale(0.0625)
+            gamePiece.position = grid.gridPosition(row: 8, col: 2)
+            grid.addChild(gamePiece)
         }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        
+        for touch in touches {
+            x_acceleration_button.name = "x_acceleration_button"
+            let positionInScene = touch.location(in: self)
+            let touchedNode = atPoint(positionInScene)
+            if let name = touchedNode.name
+            {
+                if name == "x_acceleration_button"
+                {
+                    print("accelerate")
+                    if racecar.x_acceleration < 1 {
+                        racecar.x_acceleration += 1
+                        print(racecar.x_acceleration)
+                    }
+                }
+                
+                if name == "x_deacceleration_button"
+                {
+                    print("deaccelerate")
+                    if racecar.x_acceleration > -1{
+                        racecar.x_acceleration -= 1
+                        print(racecar.x_acceleration)
+                    }
+                }
+            }
+
+            
+
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
         
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
+        
+        for touch in touches {
+            
+//            print("touches ended")
+//            let location = touch.location(in: view)
+//            print(location)
+//            if x_acceleration_button.contains(location) {
+//                print("tapped!")
+//            }
+            
+        
+        
+//        let touch:UITouch = touches.anyObject()! as UITouch
+            
+//            x_acceleration_button.name = "x_acceleration_button"
+//            x_acceleration_button.isUserInteractionEnabled = false
+//            let positionInScene = touch.location(in: self)
+//            let touchedNode = self.atPoint(positionInScene)
+//            
+//            if let name = touchedNode.name
+//            {
+//                if name == "x_acceleration_button"
+//                {
+//                    print("Touched")
+//                }
+//            }
         }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
     }
+    
+
 }
