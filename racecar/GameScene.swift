cbf42196908f80d00799e97048fe7574ecf26170
Mitchell <mitchell@shopkeepery>
2017-03-24 23:03:18 -0700

    praise the sun

--- racecar/GameScene.swift
+++ racecar/GameScene.swift
@@ -9,35 +9,49 @@
 import SpriteKit
 import GameplayKit
 
-let rows = 13
-let cols = 10
+let rows = 33
+let cols = 19
 
 
 class GameScene: SKScene {
     
  
-    let x_acceleration_button = SKSpriteNode(color: SKColor.red, size: CGSize(width: 175, height: 88))
+    let x_acceleration_button = SKSpriteNode(imageNamed: "right_red")
     
-    let x_deacceleration_button = SKSpriteNode(color: SKColor.blue, size: CGSize(width: 175, height: 88))
+    let x_deacceleration_button = SKSpriteNode(imageNamed: "left_blue")
     
     
-    let y_acceleration_button = SKSpriteNode(color: SKColor.red, size: CGSize(width: 175, height: 88))
+    let y_acceleration_button = SKSpriteNode(imageNamed: "up_red")
     
-    let y_deacceleration_button = SKSpriteNode(color: SKColor.blue, size: CGSize(width: 175, height: 88))
+    let y_deacceleration_button = SKSpriteNode(imageNamed: "down_blue")
     
 
     
-    let racecar = Racecar(x_pos: 5, y_pos: 6)
+    let racecar = Racecar(x_pos: 16, y_pos: 20)
     
     let x_velocity_display = SKLabelNode()
     let y_velocity_display = SKLabelNode()
     
-    let grid = Grid(blockSize: 60.0, rows:rows, cols:cols)
+    let grid = Grid(blockSize: 35.0, rows:rows, cols:cols)
     let gamePiece = SKSpriteNode(imageNamed: "Spaceship")
-    //let projected_velocity = SKSpriteNode(imageNamed: "red")
+    let projected_velocity = SKSpriteNode(imageNamed: "red")
+    let vroom = SKSpriteNode(imageNamed: "red")
+    
+    var obstacles:[(x: Int, y: Int)] = []
+
+    
+
     
     override func didMove(to view: SKView) {
         
+        for i in 6...(rows-6) {
+            
+            for j in 4...(cols-6) {
+                
+                obstacles.append(x: i, y: j)
+                
+            }
+        }
         
         x_velocity_display.text = "0"
         x_velocity_display.fontSize = 65
@@ -47,50 +61,80 @@ class GameScene: SKScene {
         y_velocity_display.fontSize = 65
         y_velocity_display.fontColor = SKColor.white
         
-        x_velocity_display.position = CGPoint(x: -self.frame.width / 2 + 50, y: -1 * self.frame.height / 2 + 50)
-        y_velocity_display.position = CGPoint(x: self.frame.width / 12 , y: -1 * self.frame.height / 2 + 50)
+//        x_velocity_display.position = CGPoint(x: -self.frame.width / 2 + 50, y: -1 * self.frame.height / 2 + 50)
+//        y_velocity_display.position = CGPoint(x: self.frame.width / 12 , y: -1 * self.frame.height / 2 + 50)
         
 
         
-        self.addChild(x_velocity_display)
-        self.addChild(y_velocity_display)
+//        x_velocity_display.position = CGPoint(x: -self.frame.width / 2 + 50, y: -1 * self.frame.height / 2 + 50)
+//        y_velocity_display.position = CGPoint(x: self.frame.width / 12 , y: -1 * self.frame.height / 2 + 50)
+//        x_acceleration_button.position = CGPoint(x: -self.frame.width / 4, y: -1 * self.frame.height / 2 + 50)
+//        x_deacceleration_button.position = CGPoint(x: -self.frame.width / 12, y: -1 * self.frame.height / 2 + 50)
+//        y_acceleration_button.position = CGPoint(x: self.frame.width / 4, y: -1 * self.frame.height / 2 + 50)
+//        y_deacceleration_button.position = CGPoint(x: self.frame.width / 2 - 75, y: -1 * self.frame.height / 2 + 50)
         
-        let x_acceleration_button = SKSpriteNode(color: SKColor.red, size: CGSize(width: 90, height: 90))
-        x_acceleration_button.position = CGPoint(x: -self.frame.width / 4
-            , y: -1 * self.frame.height / 2 + 50)
+        x_velocity_display.position = CGPoint(x: -self.frame.width / 2 + 50, y: -1 * self.frame.height / 2 + 50)
+        y_velocity_display.position = CGPoint(x: -self.frame.width / 4, y: -1 * self.frame.height / 2 + 50)
+        x_acceleration_button.position = CGPoint(x: -self.frame.width / 12, y: -1 * self.frame.height / 2 + 50)
+        x_deacceleration_button.position = CGPoint(x: self.frame.width / 12 , y: -1 * self.frame.height / 2 + 50)
+        y_acceleration_button.position = CGPoint(x: self.frame.width / 4, y: -1 * self.frame.height / 2 + 50)
+        y_deacceleration_button.position = CGPoint(x: self.frame.width / 2 - 75, y: -1 * self.frame.height / 2 + 50)
+        
+        
+        
+        x_acceleration_button.setScale(0.35)
+        //x_acceleration_button.position = CGPoint(x: -self.frame.width / 4, y: -1 * self.frame.height / 2 + 50)
         x_acceleration_button.name = "x_acceleration_button"
         
-        let x_deacceleration_button = SKSpriteNode(color: SKColor.blue, size: CGSize(width: 90, height: 90))
-        x_deacceleration_button.position = CGPoint(x: -self.frame.width / 12, y: -1 * self.frame.height / 2 + 50)
+        x_deacceleration_button.setScale(0.35)
+        //x_deacceleration_button.position = CGPoint(x: -self.frame.width / 12, y: -1 * self.frame.height / 2 + 50)
         x_deacceleration_button.name = "x_deacceleration_button"
         
-        let y_acceleration_button = SKSpriteNode(color: SKColor.red, size: CGSize(width: 90, height: 90))
-        y_acceleration_button.position = CGPoint(x: self.frame.width / 4, y: -1 * self.frame.height / 2 + 50)
+        y_acceleration_button.setScale(0.35)
+        //y_acceleration_button.position = CGPoint(x: self.frame.width / 4, y: -1 * self.frame.height / 2 + 50)
         y_acceleration_button.name = "y_acceleration_button"
         
-        let y_deacceleration_button = SKSpriteNode(color: SKColor.blue, size: CGSize(width: 90, height: 90))
-        y_deacceleration_button.position = CGPoint(x: self.frame.width / 2 - 75, y: -1 * self.frame.height / 2 + 50)
+        y_deacceleration_button.setScale(0.35)
+        //y_deacceleration_button.position = CGPoint(x: self.frame.width / 2 - 75, y: -1 * self.frame.height / 2 + 50)
         y_deacceleration_button.name = "y_deacceleration_button"
         
         
+        let vroom = SKSpriteNode(color: SKColor.red, size: CGSize(width: 90, height: 90))
+        vroom.position = CGPoint(x: 0, y: self.frame.height / 2 - 50)
+        vroom.name = "vroom"
         
+        
+        self.addChild(x_velocity_display)
+        self.addChild(y_velocity_display)
         self.addChild(x_acceleration_button)
         self.addChild(x_deacceleration_button)
         self.addChild(y_acceleration_button)
         self.addChild(y_deacceleration_button)
+        self.addChild(vroom)
 
         
-        grid?.position = CGPoint (x:frame.midX, y:frame.midY)
+        grid?.position = CGPoint (x:frame.midX, y:frame.midY+50)
         addChild(grid!)
         
         gamePiece.setScale(0.0625)
         gamePiece.position = (grid?.gridPosition(row:  racecar.y_position, col:  racecar.x_position))!
+        gamePiece.zPosition = 20
         
-//        projected_velocity.setScale(0.0625)
-//        projected_velocity.position = (grid?.gridPosition(row:  (racecar.x_position - racecar.x_acceleration), col:  (racecar.y_position - racecar.y_acceleration)))!
+        for obstacle in obstacles {
+            
+            let current_obstacle = SKSpriteNode(color: SKColor.red, size: CGSize(width: 30, height: 30))
+            current_obstacle.position = (grid?.gridPosition(row:  obstacle.x, col:  obstacle.y))!
+            grid?.addChild(current_obstacle)
+            
+        }
         
+        projected_velocity.setScale(0.0625)
+        projected_velocity.position = (grid?.gridPosition(row:  racecar.y_position , col: racecar.x_position))!
+        gamePiece.zPosition = 19
+        
+        grid?.addChild(projected_velocity)
         grid?.addChild(gamePiece)
-        //grid?.addChild(projected_velocity)
+
         
         
     }
@@ -105,7 +149,8 @@ class GameScene: SKScene {
 
         gamePiece.position = (grid?.gridPosition(row:  racecar.y_position , col: racecar.x_position))!
         
-        grid!.removeAllChildren()
+        
+        gamePiece.removeFromParent()
         grid?.addChild(gamePiece)
         //grid?.addChild(projected_velocity)
         
@@ -164,18 +209,18 @@ class GameScene: SKScene {
                         racecar.y_acceleration -= 1
                         print(racecar.y_acceleration)
                     }
+                } else if name == "vroom" {
+                    move()
                 }
-            } else {
-                move()
             }
-
-            
-
         }
     }
 
     
     override func update(_ currentTime: TimeInterval) {
+        
+        
+        print()
         // Called before each frame is rendered
         //x_velocity_display.color = SKColor.red
         if racecar.x_acceleration == 1 {
@@ -196,6 +241,9 @@ class GameScene: SKScene {
         
         x_velocity_display.text = String(racecar.x_velocity + racecar.x_acceleration)
         y_velocity_display.text = String(racecar.y_velocity + racecar.y_acceleration)
+        
+        projected_velocity.position = (grid?.gridPosition(row:  racecar.y_position - racecar.y_velocity - racecar.y_acceleration, col: racecar.x_position + racecar.x_velocity + racecar.x_acceleration))!
+        
 
     }
     
