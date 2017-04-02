be4f4d0b70a9990132b89d7784f6705bc13ceb6c
Mitchell <mitchell@shopkeepery>
2017-04-01 18:09:42 -0700

    nice course object

--- racecar/EasyRaceScene.swift
+++ racecar/EasyRaceScene.swift
@@ -23,8 +23,15 @@ class EasyRaceScene: Course {
     init() {
         super.init(size: CGSize(), difficulty: "hard")
         grid = Grid(blockSize: CGFloat(35.0), rows: 40, cols: 25)
-        racecar = Racecar(x_pos: 20, y_pos: 22)
-        obstacles = []
+        racecar = Racecar(x_pos: 22, y_pos: 22)
+        for j in 5...20{
+            
+            for i in 5...35 {
+                
+                obstacles.append(x: i, y: j)
+                
+            }
+        }
     }
     
     required init?(coder aDecoder: NSCoder) {
